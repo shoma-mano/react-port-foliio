1913b4d3845934a768add77909b48b06f55ae1a7
manowaraiuma <ms2geki@gmail.com>
2021-12-27 23:06:07 +0900

    historyページ修正

--- components/history/LeftSideHistory.tsx
+++ components/history/LeftSideHistory.tsx
@@ -7,83 +7,73 @@ export const LeftSideHistory = () =>{
     const histories=[
         {
             time:'2003-2009',
-            title:'茨城県常陸大宮市立'
+            title:'小学校時代',
+            sentence:'全校生徒60人という学校で自然の中に囲まれてのびのびと育つ。'
         }
     ]
 
     return (
-        <Box>
-            <Box
-                fontFamily={'theme'}
-                border={'solid 1px'}
-                boxShadow={'md'}
-                borderColor={'rgba(0, 0, 0, 0.15)'}
-                width={'240px'}
-                height={'90px'}
-                borderRadius={'10px'}
-                position={'relative'}
-                bg={'white'}
-                mr={'20px'}
-            >
-                {/*dot on center line*/}
-                <Box
-                    right={'-29px'}
-                    position={'absolute'}
-                    bg={'#9e9e9e'}
-                    mt={"39px"}
-                    borderRadius={'50px'}
-                    height={'12px'}
-                    width={'12px'}
-                ></Box>
-                <Text
-                    textAlign={'center'}
-                    fontWeight={'600'}
-                    color={'#525252'}
-                    fontSize={'22px'}
-                    marginTop={"3px"}
-                    position={"absolute"}
-                    left={"260px"}
-                    width={"180px"}
-                    top={"28px"}
-                >
-                    2003-2009
-                </Text>
-                {/*<Text*/}
-                {/*    textAlign={'center'}*/}
-                {/*    fontWeight={'600'}*/}
-                {/*    color={'#000000d4'}*/}
-                {/*    fontSize={'15px'}*/}
-                {/*    marginTop={"10px"}*/}
-                {/*>*/}
-                {/*    2003-2009*/}
-                {/*</Text>*/}
-                <Text
-                    textAlign={'center'}
-                    fontWeight={'600'}
-                    color={'#000000d4'}
-                    fontSize={'18px'}
-                    marginTop={"10px"}
-                >
-                    小学校時代
-                </Text>
-                {/*<Text*/}
-                {/*    textAlign={'center'}*/}
-                {/*    fontWeight={'600'}*/}
-                {/*    color={'#000000d4'}*/}
-                {/*    fontSize={'12px'}*/}
-                {/*    marginTop={"3px"}*/}
-                {/*>*/}
-                {/*    2003-2009*/}
-                {/*</Text>*/}
-                <Text
-                    textAlign={'center'}
-                    fontWeight={'600'}
-                    color={'#000000a8'}
-                    marginTop={"10px"}
-                    fontFamily={selectedLanguage === 'ja' ? 'monospace' : "'Rubik', sans-serif;"}
-                    mt={'5px'}
-                >全校生徒60人という学校で自然の中に囲まれてのびのびと育つ。</Text>
-            </Box>
+        <Box
+        mt={"20px"}
+        >
+            {histories.map(v=>{
+                return(
+                    <Box
+                        key={v.time}
+                        fontFamily={'theme'}
+                        border={'solid 1px'}
+                        boxShadow={'md'}
+                        borderColor={'rgba(0, 0, 0, 0.15)'}
+                        width={'240px'}
+                        height={'90px'}
+                        borderRadius={'10px'}
+                        position={'relative'}
+                        bg={'white'}
+                        mr={'20px'}
+                    >
+                        {/*dot on center line*/}
+                        <Box
+                            right={'-29px'}
+                            position={'absolute'}
+                            bg={'#9e9e9e'}
+                            mt={"39px"}
+                            borderRadius={'50px'}
+                            height={'12px'}
+                            width={'12px'}
+                        ></Box>
+                        <Text
+                            textAlign={'center'}
+                            fontWeight={'600'}
+                            color={'#525252'}
+                            fontSize={'22px'}
+                            marginTop={"3px"}
+                            position={"absolute"}
+                            left={"260px"}
+                            width={"180px"}
+                            top={"28px"}
+                        >
+                            {v.time}
+                        </Text>
+                        <Text
+                            textAlign={'center'}
+                            fontWeight={'600'}
+                            color={'#000000d4'}
+                            fontSize={'18px'}
+                            marginTop={"10px"}
+                        >
+                            {v.title}
+                        </Text>
+                        <Text
+                            textAlign={'center'}
+                            fontWeight={'600'}
+                            color={'#000000a8'}
+                            marginTop={"10px"}
+                            fontFamily={selectedLanguage === 'ja' ? 'monospace' : "'Rubik', sans-serif;"}
+                            mt={'5px'}
+                        >{v.sentence}</Text>
+                    </Box>
+                )
+            })}
         </Box>
     )
 }
--- components/history/RightSideHistory.tsx
+++ components/history/RightSideHistory.tsx
@@ -1,43 +1,80 @@
 import { Box, Text } from "@chakra-ui/react";
+import { useContext } from "react";
+import { languageContext } from "../../pages/_app";
 
-export const RightSideHistory=()=>
-    <Box>
+export const RightSideHistory = () => {
+    const {selectedLanguage} = useContext(languageContext);
+    const histories = [
+        {
+            time: '2003-2009',
+            title: '小学校時代',
+            sentence: '全校生徒60人という学校で自然の中に囲まれてのびのびと育つ。'
+        }
+    ]
+
+    return (
         <Box
-            mt={"350px"}
-            fontFamily={'theme'}
-            border={'solid 1px'}
-            boxShadow={'md'}
-            borderColor={'rgba(0, 0, 0, 0.15)'}
-            width={'240px'}
-            height={'120px'}
-            borderRadius={'10px'}
-            position={'relative'}
-            bg={'white'}
-            ml={'20px'}
+            mt={"130px"}
         >
-            <Box
-                left={'-29px'}
-                position={'absolute'}
-                bg={'#9e9e9e'}
-                mt={"54px"}
-                borderRadius={'50px'}
-                height={'12px'}
-                width={'12px'}
-            ></Box>
-            <Text
-                textAlign={'center'}
-                fontWeight={'600'}
-                color={'#000000d4'}
-                fontSize={'20px'}
-                marginTop={"10px"}
-            >
-                2012-2015
-            </Text>
-            <Text color={'#000000d4'}
-                  height={'50%'}
-                  textAlign={"center"}
-            >
-                水城高等</Text>
+            {histories.map(v => {
+                return (
+                    <Box
+                        key={v.time}
+                        fontFamily={'theme'}
+                        border={'solid 1px'}
+                        boxShadow={'md'}
+                        borderColor={'rgba(0, 0, 0, 0.15)'}
+                        width={'240px'}
+                        height={'90px'}
+                        borderRadius={'10px'}
+                        position={'relative'}
+                        bg={'white'}
+                        m={'20px'}
+                    >
+                        {/*dot on center line*/}
+                        <Box
+                            left={'-29px'}
+                            position={'absolute'}
+                            bg={'#9e9e9e'}
+                            mt={"39px"}
+                            borderRadius={'50px'}
+                            height={'12px'}
+                            width={'12px'}
+                        ></Box>
+                        <Text
+                            textAlign={'center'}
+                            fontWeight={'600'}
+                            color={'#525252'}
+                            fontSize={'22px'}
+                            marginTop={"3px"}
+                            position={"absolute"}
+                            right={"260px"}
+                            width={"180px"}
+                            top={"28px"}
+                        >
+                            {v.time}
+                        </Text>
+                        <Text
+                            textAlign={'center'}
+                            fontWeight={'600'}
+                            color={'#000000d4'}
+                            fontSize={'18px'}
+                            marginTop={"10px"}
+                        >
+                            {v.title}
+                        </Text>
+                        <Text
+                            textAlign={'center'}
+                            fontWeight={'600'}
+                            color={'#000000a8'}
+                            marginTop={"10px"}
+                            fontFamily={selectedLanguage === 'ja' ? 'monospace' : "'Rubik', sans-serif;"}
+                            mt={'5px'}
+                        >{v.sentence}</Text>
+                    </Box>
+                )
+            })}
         </Box>
-    </Box>
+    )
 
+}
