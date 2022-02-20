61aca16c828311509914e53a14409d1a6977af8b
r-unit-mano <ms2geki@gmail.com>
2022-02-20 18:55:12 +0900

    アイコン変更

--- components/left_side_bar/LeftSideBar.tsx
+++ components/left_side_bar/LeftSideBar.tsx
@@ -114,7 +114,7 @@ export const LeftSideBar = ({ onClick }: { onClick: () => void }) => {
                     border={'1px solid'}
                     borderColor={'#00000021'}
                     bg={'rgb(205 205 205)'}
-                    src='/mychemical.png'
+                    src='/lofi.png'
                     filter={'brightness(1.2)'}
                 ></Image>
                 <Text textAlign={'center'} fontSize={'20px'} fontWeight={'700'} mt={'32px'}>
