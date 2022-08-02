67098149ed07b404d910aeed1c52d98941e620dd
mano <ms2geki@gmail.com>
2022-08-02 20:26:05 +0900

    eliminate render-blocking resources

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { ColorModeScript } from '@chakra-ui/react';
 import theme from '../theme';
+import * as React from "react";
 
 class MyDocument extends NextDocument {
     render() {
@@ -10,6 +11,10 @@ class MyDocument extends NextDocument {
                     <link
                         href='https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Rubik&display=swap'
                         rel='stylesheet'
+                    />
+                    <link
+                        rel='stylesheet'
+                        href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/dark.min.css'
                     />
                     <link rel='shortcut icon' href='/lofi-icon.png' />
                 </Head>
